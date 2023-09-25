aa4147d7def4375de1fe39feb4e7a1f404407700
Truong Duc Ngoc <truongducngoc@codelovers.vn>
2023-09-25 17:41:46 +0700

    update code 202309-25

--- backend/resources/views/common/effect/error.blade.php
+++ backend/resources/views/common/effect/error.blade.php
@@ -1,12 +1,47 @@
 <style>
     #error-alert{
         min-width: 300px;
+        width: auto;
         position: fixed;
         top: 50px;
         right: -400px;
         transition: right 0.5s ease-out;
         z-index: 9999;
-        margin-bottom: 10px
+        margin-bottom: 10px;
+    }
+
+    .alert {
+        background: #fff;
+        border-left: 10px solid #fc6661;
+        border-top: 2px solid #fc6661;
+        border-right: 2px solid #fc6661;
+        border-bottom: 2px solid #fc6661;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        padding: 16px 0;
+    }
+
+    .noti {
+        flex: 1;
+        padding-right: 20px;
+    }
+
+    .noti h5 {
+        color: #000;
+    }
+
+    .noti p {
+        margin-bottom: 0px;
+        color: #000;
+    }
+
+    .icon {
+        padding: 0 20px 0 10px;
+    }
+
+    .icon i {
+        color: #fc6661;
     }
 
     .error-message {
@@ -20,12 +55,17 @@
 </style>
 
 @if ($errors->any())
-    <dir id="error-alert">
+    <div id="error-alert">
         @foreach ($errors->all() as $error)
             <div class="alert alert-danger">
-                <h5 class="text-danger">Opps!</h5>
-                <p class="error-message">{{ $error }}</p>
+                <div class="icon">
+                    <i class="ti ti-x"></i>
+                </div>
+                <div class="noti">
+                    <h5>Error!</h5>
+                    <p class="error-message">{{ $error }}</p>
+                </div>
             </div>
         @endforeach
-    </dir>
+    </div>
 @endif
--- backend/resources/views/common/effect/success.blade.php
+++ backend/resources/views/common/effect/success.blade.php
@@ -1,15 +1,43 @@
 <style>
     #success-alert {
-        min-width: 300px;
+        min-width: 340px;
+        width: auto;
         position: fixed;
         top: 50px;
         right: -400px;
-        transition: right 0.5s ease-out;
+        background: #fff;
         z-index: 9999;
+        transition: right 0.5s ease-out;
+        border-left: 10px solid #a1f2e3;
+        border-top: 2px solid #a1f2e3;
+        border-right: 2px solid #a1f2e3;
+        border-bottom: 2px solid #a1f2e3;
+        display: flex;
+        align-items: center;
+        padding: 16px 0;
     }
 
-    #success-alert p {
+    .noti {
+        flex: 1;
+        padding-right: 20px;
+    }
+
+    .noti h5 {
+        color: #000;
+        font-weight: bold;
+    }
+
+    .noti p {
         margin-bottom: 0px;
+        color: #000;
+    }
+
+    .icon {
+        padding: 0 20px 0 10px;
+    }
+
+    .icon i {
+        font-size: 26px
     }
 
     #success-alert.show {
@@ -19,7 +47,12 @@
 
 @if (session('success'))
     <div class="alert alert-success" id="success-alert">
-        <h5 class="text-success">Yeah!</h5>
-        <p>{{ session('success') }}</p>
+        <div class="icon">
+            <i class="ti ti-check"></i>
+        </div>
+        <div class="noti">
+            <h5>Success!</h5>
+            <p>{{ session('success') }}</p>
+        </div>
     </div>
 @endif
